fb7ffddb42f30d174870a53ea7eb9a0fedbe04bc
Diego FG <femenias@gmail.com>
2025-05-05 14:48:43 -0300

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -123,7 +123,7 @@ function App() {
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigate to={`/${nanoid()}`} />} />
+      <Route path="/" element={<Navigate to={`/chat`} />} />
       <Route path="/:room" element={<App />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
